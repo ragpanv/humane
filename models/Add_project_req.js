1372a756378aad44340261dd7c1a3982d3ef8d41
ragpanv <72090309+ragpanv@users.noreply.github.com>
2022-07-14 02:29:47 +0530

    done

---
+++ models/Add_project_req.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+ 
+const addProjectSchema = new Schema({
+  Name: {
+    type: String,
+    required: true,
+  },
+  Desc: {
+    type: String,
+    required: true,
+  },
+  Email: {
+    type: String,
+    required: true,
+  },
+  MobileNo: {
+    type: String,
+    required: true,
+  },
+  Amount: {
+    type: Number,
+    required: true,
+  },
+  Adhaar: {
+    type: String,
+    required: true,
+  },
+  ProjectHolder: {
+    type: String,
+    required: true,
+  },
+  Link:{
+    type:String,
+    required: true,
+  },
+  Bid: {
+    type: String,
+    required: true,
+  },
+  AvailableAmount: {
+    type: Number,
+    required: true,
+  },
+  RequiredAmount: {
+    type: Number,
+    required: true,
+  }
+});
+ 
+const add_projects_req= mongoose.model('projects_to_donate', addProjectSchema);
+module.exports = add_projects_req;
